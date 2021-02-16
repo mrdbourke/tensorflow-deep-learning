86506544abf87d6e1c9589459c69c72e9c738f15
Daniel Bourke <danielbourke@icloud.com>
2021-02-16 12:11:35 +1000

    Update helper_functions.py

--- extras/helper_functions.py
+++ extras/helper_functions.py
@@ -248,3 +248,23 @@ def unzip_data(filename):
   zip_ref = zipfile.ZipFile(filename, "r")
   zip_ref.extractall()
   zip_ref.close()
+
+# Walk through an image classification directory and find out how many files (images)
+# are in each subdirectory.
+import os
+
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
